2f359be93232ee2c28454633f5aef8e6a745d7ac
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2019-03-16 11:39:52 +1300

    cors

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const port = 3000
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => res.send('Giga-Whaaat??!'))
 
