893cd4d9cae8479ad2e5a3f2af9d781bbe85b2a4
Saksham <git@sakshamanand.com>
2019-03-16 09:54:16 +1300

    Temp

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express')
+const opn = require('opn');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send("Hello World!"))
+app.get('/dirWork', (req, res) => res.send(opn(directionsWork)))
+app.get('/dirHome', (req, res) => res.send(opn(directionsHome)))
+app.get('/sview', (req, res) => res.send(opn(streetView)))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//Directions Map Launch
+const originLat = -36.848386;
+const originLong = 174.765487;
+const destLat = -36.850324;
+const destLong = 174.755381;
+
+const arrivalTime = new Date('2019.03.17').getTime() / 1000;
+const departTime = 1700;
+
+const directionsWork = "https://www.google.com/maps/dir/?api=1&origin=" + originLat + "," + originLong + "&destination=" + destLat + "," + destLong + "&travelmode=transit&arrival_time=" + arrivalTime;
+const directionsHome = "https://www.google.com/maps/dir/?api=1&destination=" + originLat + "," + originLong + "&origin=" + destLat + "," + destLong + "&travelmode=transit&departure_time=" + departTime;
+
+
+//Open Street View
+const busStopLat = -36.846178;
+const BusStopLong = 174.766155;
+
+const streetView = "http://maps.google.com/maps?q=&layer=c&cbll=" + busStopLat + "," + BusStopLong;
+
+//
