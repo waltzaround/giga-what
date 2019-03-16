55753ff71317e81b281f7fe3f17d8775cb51241d
Saksham <git@sakshamanand.com>
2019-03-16 16:59:20 +1300

    Added Bus Weather

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ require('dotenv').config()
 
 const express = require('express')
 const opn = require('opn');
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const rp = require('request-promise');
 const app = express()
 const port = 3000
 
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const rp = require('request-promise');
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
-require('./messenger')(app);
 
-app.get('/', (req, res) => res.send("Hello World!"))
+
+app.get('/', async (req, res) => res.send(await getWeather(-36.846178, 174.766155)))
 app.get('/dirWork', (req, res) => res.send(opn(directionsWork)))
 app.get('/dirHome', (req, res) => res.send(opn(directionsHome)))
 app.get('/sview', (req, res) => res.send(opn(streetView)))
+app.get('/bstop', (req, res) => res.send(data))
+app.get('/weather', (req, res) => res.send(opn(weather)));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
@@ -45,3 +42,43 @@ const BusStopLong = 174.766155;
 
 const streetView = "http://maps.google.com/maps?q=&layer=c&cbll=" + busStopLat + "," + BusStopLong;
 
+//Bus Stop Locations
+// const apiKey = "AIzaSyCYaTzhSutXzDW8PYAKp2-3S3b9tPzOjd4";
+// async function getBusStopLocation() {
+//     const data = await rp({ uri: 'https://maps.googleapis.com/maps/api/directions/json?origin=-36.848386,174.765487&destination=-36.850324,174.755381&mode=transit&key=AIzaSyCYaTzhSutXzDW8PYAKp2-3S3b9tPzOjd4', json:true })
+//     console.log(data)
+//     for (const i = 0; i < 3; i++) {
+//         data.routes[0].legs[0].steps[i]
+//     }
+    
+
+//     return location
+// }
+// getBusStopLocation()
+
+
+//Get Bus Weather
+const areaLat = -36.846178;
+const areaLong = 174.766155;
+const weatherAPI = "5c7e05cd01783455a05f254388393cc5";
+
+const weather = "http://api.openweathermap.org/data/2.5/weather?lat=" + areaLat + "&lon=" + areaLong + "&appid=" + weatherAPI;
+
+async function getWeather(areaLat, areaLong) {
+    const data = await rp({
+        uri: 'http://api.openweathermap.org/data/2.5/weather',
+        qs: {
+            'lat': `${areaLat}`,
+            'lon': `${areaLong}`,
+            'appid': `${weatherAPI}`,
+        },
+        json: true,
+    })
+
+  const weather = (data.weather[0].main)
+  return weather
+}
+
+(async () => {
+    console.log(await getWeather(-36.846178, 174.766155))
+})()
